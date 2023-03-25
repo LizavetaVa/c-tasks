dcc193f7d12611f1f7fc3ac0d175389a8fb501b9
Lizaveta Valynets <volynets2000@mail.ru>
2023-03-25 22:21:35 +0300

    done

---
+++ task06/Program.cs
@@ -0,0 +1,10 @@
+﻿// task6
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+int x = a % 2;
+
+if (x == 0)
+    Console.WriteLine("Введенное число является четным.");
+else
+    Console.WriteLine("Введенное число является нечетным.");
